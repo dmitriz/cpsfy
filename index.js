b7f9b848cf85267dc95e939f1dfa35d035f2b8c5
dmitriz <dmitri14@gmail.com>
2019-05-19 20:16:01 +0700

    fix tests

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-/* 
- * Pipeline Operator
- * based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Pipeline_operator
- * but additionally allows passing a chain of functions
- * 
- * write pipe chain  
- *		x |> f1 |> f2 |> ... |> fn
- * compactly as single function call
- * 		pipeline(x)(f1, f2, ..., fn)
- * which is equivalent to
- * 		fn(...f2(f1(x))...)
- */
-// const pipeline = (...args) => (...fns) =>
-// 	...fns.reduce(
-// 		(acc, fn) => fn(acc),
-// 		...args
-// 	)
-
-
 // [[a]] -> CPS
 const of = (...arrays) => (...cbs) => {
 	arrays.forEach(
@@ -45,17 +26,40 @@ const chain = (...cpsFns) => cpsFun =>
 		)
 	)
 
+
+// TODO: fix is needed to avoid object mutation
 // add method to object by delegating to curried function applied to it
-// const fn2method => (obj, name, fn) => {
-// 	obj[name] = (...args) => fn(...args)(obj)
-// 	return obj
-// }
+const fn2method = (obj, name, fn) => {
+	obj[name] = (...args) => fn(...args)(obj)
+	return obj
+}
 
 // // CPS adds the methods to its argument
-// const methods = { map, chain }
-// const CPS = cpsFun => Object.keys(methods).reduce(
-// 	(acc, fnName) => fn2method(acc, fnName, methods[fnName])
-// 	cpsFun
-// )
+const methods = { map, chain }
+const CPS = cpsFun => Object.keys(methods).reduce(
+	(acc, fnName) => fn2method(acc, fnName, methods[fnName]),
+	cpsFun
+)
+
+
+// TODO: fix implementation
+/**
+ * Pipeline Operator
+ * based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Pipeline_operator
+ * but additionally allows passing a chain of functions
+ * 
+ * write pipe chain  
+ *		x |> f1 |> f2 |> ... |> fn
+ * compactly as single function call
+ * 		pipeline(x)(f1, f2, ..., fn)
+ * which is equivalent to
+ * 		fn(...f2(f1(x))...)
+ */
+// const pipeline = (...args) => (...fns) =>
+// 	...fns.reduce(
+// 		(acc, fn) => fn(acc),
+// 		...args
+// 	)
+
 
 module.exports = { of, map, chain }
