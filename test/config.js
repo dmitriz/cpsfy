ab90d9099009f0a4a2c2b5f912e8adb6e86a768a
dmitriz <dmitri14@gmail.com>
2019-06-06 16:17:50 +0700

    fix(test): prevent vulnerable params inside require

--- test/config.js
+++ test/config.js
@@ -1,8 +1,8 @@
 const test = process.env.TEST || 'tape'
 
-const test_patch = ({
-	'tape': require('./helpers/tape-patched'),
-	'ava': require('./helpers/ava-patched'),
-})[test] 
+const getTestPatch = ({
+	'tape': _ => require('./helpers/tape-patched'),
+	'ava': _ => require('./helpers/ava-patched'),
+})[test]
 
-module.exports = test_patch
+module.exports = getTestPatch()
