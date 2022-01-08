9e873d0ae1ed25cafdabf2034e7d5113bb1f53b3
Dmitri Zaitsev <dmitri14@gmail.com>
2022-01-08 21:42:11 +0700

    feat: 🎸 add lift2

--- index.js
+++ index.js
@@ -265,6 +265,13 @@ const ap = (...fns) => cpsFn => {
 }
 
 
+/**
+ * Lift binary function to act on values wraped inside CPS functions
+ */
+const lift2 = f => (F1, F2) => pipeline(F2)(
+  ap(map(curry2(f))(F1))
+)
+
 
 
 
@@ -297,5 +304,5 @@ const CPS = cpsFn => {
 
 module.exports = {
   curry2, pipeline,
-  of, ofN, map, chain, filter, scan, ap, CPS
+  of, ofN, map, chain, filter, scan, ap, lift2, CPS
 }
---
+++ test/lift.test.js
@@ -0,0 +1,11 @@
+const test = require('./config')
+const { lift2 } = require('..')
+
+const notCalled = () => {throw Error('I should not be called!')}
+
+test('lift2 over binary function applies to both outputs', t => {
+	const F1 = cb => {cb(42)}
+	const F2 = cb => {cb(4)}
+	const cpsNew = lift2((x,y)=>x-y)(F1,F2)
+	cpsNew(t.cis(42-4))
+})
