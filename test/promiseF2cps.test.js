5b49bfa15108cd3bd5ab7bc31ce138ddbb2e1154
Dmitri Zaitsev <dmitri14@gmail.com>
2022-12-26 19:10:02 +0700

    test: 💍 downgrade to tape@4

--- test/promiseF2cps.test.js
+++ test/promiseF2cps.test.js
@@ -8,3 +8,4 @@ test('promiseF2cps converts fulfilled promise to cps factory returning value in 
 test('promiseF2cps converts rejected promise to cps factory returning value in the 2nd callback', async t=>{
 	await promiseF2cps(x=>Promise.reject(x))(3)(null,t.cis(3))
 })
+
